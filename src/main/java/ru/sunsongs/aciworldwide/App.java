edc527a69b2633244aca72ad313ccb6804004153
kraken <root@sunsongs.ru>
2014-02-23 01:38:47 +0600

    app skeleton

---
+++ src/main/java/ru/sunsongs/aciworldwide/App.java
@@ -0,0 +1,12 @@
+package ru.sunsongs.aciworldwide;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+class App{
+	public static void main(String ... args){
+        throw new UnsupportedOperationException("Don't anything to see. Please see a tests and Frequency class");
+	}
+}
